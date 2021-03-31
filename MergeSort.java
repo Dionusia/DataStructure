b05ad7757ae33a9734396a7d9284341f1c65d6fb
Chriserdas <up1072543@upnet.gr>
2021-03-31 20:01:22 +0300

    merge

--- MergeSort.java
+++ MergeSort.java
@@ -6,8 +6,11 @@ public class MergeSort {
         try{
             FiletoArray fta = new FiletoArray(filename, field);
             double array[] = fta.getField();
-            merge(array);
+            mergeSort(array);
             
+            for(double i : array) {
+                System.out.println(i);
+            }
               
         }catch(IOException e){
             e.printStackTrace();
@@ -15,29 +18,12 @@ public class MergeSort {
 
     }
 
-    public static void mergeSort( double[] array,int left_size, int right_size){
-       
-        int mid = array.length/ 2;
-        double  left_array[] = new double[mid];
-        double  right_array[] = new double[array.length-mid];
-        
-        //copy data to sub arrays
-        int k=0;
+    public static void merge(double[] left_array,double[] right_array,double[] array){
 
-        for(int i = 0;i<array.length;++i){
-            if(i<mid){
-                left_array[i] = array[i];
-            }
-            else{
-                right_array[k] = array[i];
-                k = k+1;
-            }
-        }
+        int i=0;
+        int left_counter=0;  int right_counter=0;
+        while(left_counter < left_array.length && right_counter < right_array.length){
 
-       int i=0;
-      int left_counter=0;  int right_counter=0;
-        while(left_counter<left_size && right_counter<right_size){
-          
             if(left_array[left_counter]<right_array[left_counter]){
                 array[i++] = left_array[left_counter++];
             }
@@ -46,34 +32,51 @@ public class MergeSort {
             }
         }
         //this while loops checks which part of right or left has not end
-        while(left_counter<left_size){
+        while(left_counter<left_array.length){
             array[i++] = left_array[left_counter++];
         }
-        while(right_counter<right_size){
+        while(right_counter<right_array.length){
           array[i++] = right_array[right_counter++];
         }
     }
 
-    public static void merge(double array[]){
-        if (array.length <= 1){ return;} //checks if there is one element in the array
         
-        int mid = array.length/ 2;
-        double  left_array[] = new double[mid];
-        double  right_array[] = new double[array.length-mid];
-       
 
-      merge(left_array);
-      merge(right_array);
-      mergeSort(array,left_array.length,right_array.length);
-      
+    public static double[] mergeSort(double[] array){
+
+        int array_length = array.length;
+        int k = 0;
+
+        if(array_length < 2)
+            return array;
+
+        int mid_element = array_length / 2;
+
+        double[] left_array = new double[mid_element];
+        double[] right_array = new double[array.length - mid_element];
+
+        for(int i=0; i<array_length; i++){
+            if(i<mid_element){
+                left_array[i] = array[i];
+            }
+            else{
+                right_array[k] = array[i];
+                ++k;
+            }
+        }
+
+
+        mergeSort(left_array);
+        mergeSort(right_array);
+        merge(left_array, right_array,array);
+
+        return array;
     }
-
     
-     
-public static void main(String[] args) {
-    MergeSort a = new MergeSort("agn.us.txt", "Open");
-    System.out.println("Jedkeod");
-}
+         
+    public static void main(String[] args) {
+        MergeSort a = new MergeSort("agn.us.txt", "Open");
+    }
 }
 
     
--- QuickSort.java
+++ QuickSort.java
@@ -6,9 +6,10 @@ public class QuickSort {
           FiletoArray fta = new FiletoArray(filename, field);
           double array[] = fta.getField();
           quickSort(array,0,array.length); 
-          for(int i=0; i<array.length; i++)      {
-             System.out.println(array[i]);
-          }    
+
+          for(int i=0; i<array.length; i++){
+            System.out.println(array[i]);
+         }    
       }catch(IOException e){
           e.printStackTrace();
       }
@@ -20,25 +21,25 @@ public class QuickSort {
       double pivot=array[endIndex]; //make the last element as pivot element
       int i = (startIndex-1); 
         for (int j=startIndex; j<endIndex; j++) 
-        { 
+         { 
             // If current element is smaller than the pivot
             if (array[j] <= pivot) 
             { 
                // Increment index of smaller element
-                i++;  
-                double temp = array[i]; 
-                array[i] = array[j]; 
-                array[j] = temp; 
+               i++;  
+               double temp = array[i]; 
+               array[i] = array[j]; 
+               array[j] = temp; 
             } 
-        } 
+         } 
     
-        double temp = array[i+1]; 
-        array[i+1] = array[endIndex]; 
-        array[endIndex] = temp; 
-   
-        return i+1; 
+         double temp = array[i+1]; 
+         array[i+1] = array[endIndex]; 
+         array[endIndex] = temp; 
+      
+         return i+1; 
   
-       }
+   }
 
 
    public static void quickSort(double [] array,int startIndex,int endIndex){
