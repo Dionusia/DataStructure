49f6dcf4e146219c46a0911928c5a51187f7d68f
Dionusia <dionusiapsiri13@gmail.com>
2021-04-07 20:35:42 +0300

    Update CountingSort.java

--- CountingSort.java
+++ CountingSort.java
@@ -3,30 +3,33 @@ import java.util.*;
 
 public class CountingSort {
 
+      private HashMap<Double,Integer> DClose_ICloseMap = new LinkedHashMap<Double,Integer>();
+
     CountingSort(String filename, String field) {
         try{
             FiletoArray fta = new FiletoArray(filename, field);
             double array[] = fta.getField();
-            countingSort(array);
-            /*
-            double [] array1=new double[array.length]; 
+            int[] int_array= new int [array.length];
+            
             for(int i=0; i<array.length; i++){
-                double value= array[i];
-                array1[i]=value;
-            }*/
-            for(double i: array){
-                System.out.println(i);
-            }
+                DClose_ICloseMap.put(array[i],getInt(array[i]));
+                int_array[i] = getInt(array[i]);
 
-           /* SortDate order = new SortDate(array,fta.getMap());
+            }
+            countingSort(int_array);
+            
+            
+
+          /*  
+           SortDate order = new SortDate(array,fta.getMap());
 
             LinkedHashMap<String,Double> orderedMap = order.classifyDateAndField();
 
 
             for(Map.Entry<String,Double> entry : orderedMap.entrySet()) {
                 System.out.println("Key: "+ entry.getKey() + " Value: "+ entry.getValue());
-            }
-           */   
+            }*/
+             
         }catch(IOException e){
             e.printStackTrace();
         }
@@ -34,29 +37,19 @@ public class CountingSort {
     }
 
 
-    public static void countingSort(double[] array){
-        
-        //returns the nearest integer and put it into a new array
-        int [] arr=new int[array.length]; 
-        for(int i=0; i<array.length; i++){
-            int value= (int)Math.round (array[i]);
-            arr[i]=value;
-        }
+    public  void countingSort(int[] array){
         
 
         //find the maximum value on the given array
-        int max=arr[0];
-        for(int i=1; i<arr.length; i++){
-            if(max<arr[i]){
-                max=arr[i];
+        int max=array[0];
+        for(int i=1; i<array.length; i++){
+            if(max<array[i]){
+                max=array[i];
             }
         }
 
         //create an empty index array of max+1 size
         int[] index_array= new int[max+1];
-        for(int i=0; i<max; i++){
-            index_array[i]=0;
-        }
 
         //fill index array with the number of occurrences
         for(int i = 0; i < index_array.length; i++){
@@ -75,22 +68,30 @@ public class CountingSort {
           }
 
         //create output array to store sorted values 
-        int[] output_array = new int[arr.length];
+        int[] output_array = new int[array.length];
         
         //map the value of the given array with index array and put the value in the output array
-        for (int i = arr.length - 1; i >= 0; i--) {
-            output_array[index_array[arr[i]] - 1] = arr[i];
-            index_array[arr[i]]--; //decrement index array value
+        for (int i = array.length-1; i>=0; i--) {
+            output_array[index_array[array[i]] - 1] = array[i];
+            index_array[array[i]]--; //decrement index array value
         }
-        for (int i = 0; i <arr.length; ++i){
-            arr[i] = output_array[i];
+        for (int i = 0; i <array.length; ++i){
+            array[i] = output_array[i];
+            
         }
             
 }
+        
+        private int getInt(double i){
+            return (int)Math.ceil(i); 
+        
+        }
 
-    public static void main(String[] args) {
-        CountingSort a = new CountingSort("agn.us.txt", "Open");
-    }
+public static void main(String[] args) {
+       
+    CountingSort cs= new CountingSort("agn.us.txt","Close");
+ }
+
 
  
 
